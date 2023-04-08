bb15b9441b699512d9fc0963e2f6df69fdecf773
lirond101 <lirond101@gmail.com>
2023-04-08 15:22:05 +0000

    pvc

--- infrastructure/eks/k8s/cleanup.sh
+++ infrastructure/eks/k8s/cleanup.sh
@@ -1,9 +1,12 @@
 # Cleanup
-kubectl delete -f apache-app.yaml
+eksctl delete addon --cluster $1 --name aws-ebs-csi-driver --preserve
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.0/deploy/static/provider/aws/deploy.yaml
+
+kubectl delete -f kuar-app.yaml
 kubectl delete -f jenkins-app.yaml
-kubectl delete ingress --all
+
+kubectl delete ingress kuar-ingress -n kandula
 kubectl delete ingress devtools-ingress -n jenkins
-kubectl delete ingress ingress-nginx -n ingress-nginx
-kubectl delete namespace ingress-nginx
+
 kubectl delete namespace jenkins
-kubectl delete namespace check
+kubectl delete namespace kandula
--- infrastructure/eks/k8s/setup.sh
+++ infrastructure/eks/k8s/setup.sh
@@ -11,16 +11,28 @@ eksctl create iamidentitymapping \
     --no-duplicate-arns
 eksctl get iamidentitymapping --cluster $1 --region=us-east-1
 
-# # Install NGINX ingress conroller 
+# Install NGINX ingress conroller 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.0/deploy/static/provider/aws/deploy.yaml
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=120s
 
+# Configure ebs-csi and iam role for it
+eksctl create addon --name aws-ebs-csi-driver --cluster $1 --service-account-role-arn arn:aws:iam::902770729603:role/AmazonEKS_EBS_CSI_DriverRole --force
+eksctl create iamserviceaccount \
+  --name ebs-csi-controller-sa \
+  --namespace kube-system \
+  --cluster my-cluster \
+  --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy \
+  --approve \
+  --role-only \
+  --role-name AmazonEKS_EBS_CSI_DriverRole
+
 # Create jenkins and apache appications
-kubectl apply -f jenkins-app.yaml
 kubectl apply -f jenkins-sa.yaml
+kubectl apply -f jenkins-pv-claim.yaml
+kubectl apply -f jenkins-app.yaml
 
 kubectl create namespace kandula
 kubectl apply -f kuar-app.yaml
