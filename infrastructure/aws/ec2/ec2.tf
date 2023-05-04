9eb208b9573c61d672b85811bd5cd30b0b8a100b
lirond101 <lirond101@gmail.com>
2023-05-04 05:42:08 +0000

    use module ec2 on github, remove local key

--- infrastructure/aws/ec2/ec2.tf
+++ infrastructure/aws/ec2/ec2.tf
@@ -13,19 +13,43 @@ data "aws_ssm_parameter" "ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+data "aws_vpc" "selected" {
+  cidr_block = var.vpc_cidr_block
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  tags = {
+    Tier = "Private"
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  tags = {
+    Tier = "Public"
+  }
+}
+
 # INSTANCES #
 module "my_ec2" {
     depends_on = [
-    module.my_vpc,
     aws_iam_instance_profile.instance_profile
   ]
-  source                             = "../modules/ec2"
+  source  = "app.terraform.io/opsschool-lirondadon/ec2/aws"
+  version = "1.0.2"
   
   # VPC
-  vpc_id                             = module.my_vpc.vpc_id
-  vpc_cidr_block                     = module.my_vpc.vpc_cidr_block
-  vpc_public_subnets                 = module.my_vpc.vpc_public_subnets
-  vpc_private_subnets                = module.my_vpc.vpc_private_subnets
+  vpc_id                             = data.aws_vpc.selected.id
+  vpc_cidr_block                     = var.vpc_cidr_block
+  vpc_public_subnets                 = data.aws_subnets.public.ids
+  vpc_private_subnets                = data.aws_subnets.private.ids
 
   # BASTION
   instance_count_bastion             = length(var.public_subnets)
@@ -38,8 +62,8 @@ module "my_ec2" {
   # CONSUL
   instance_count_consul             = 0
   # instance_count_consul             = var.instance_count_consul_servers
-  # ami_consul                        = lookup(var.ubuntu_18_region_based_ami, var.aws_region)
-  # iam_instance_profile_consul       = aws_iam_instance_profile.instance_profile.name
+  ami_consul                        = lookup(var.ubuntu_18_region_based_ami, var.aws_region)
+  iam_instance_profile_consul       = aws_iam_instance_profile.instance_profile.name
   # key_name                          = var.key_name
   # user_data_consul                = file("${path.module}/scripts/consul-server.sh")
 
--- infrastructure/aws/ec2/providers.tf
+++ infrastructure/aws/ec2/providers.tf
@@ -4,14 +4,6 @@
 
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4.3"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = ">=2.18.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.58.0"
@@ -19,14 +11,6 @@ terraform {
     local = {
       source  = "hashicorp/local"
       version = "~> 2.4.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2.1"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
     }
   }
   required_version = ">= 1.1.0"
